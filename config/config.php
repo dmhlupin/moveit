59c88191bf298d2306e7bca370f92fb12b2de76a
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-10-07 22:48:22 +0300

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -6,8 +6,8 @@ define('LAYOUTS_DIR', 'layouts/');
 
 // DB
 define('HOST', 'localhost:3307');
-define('USER', 'root');
-define('PASS', '123456');
+define('USER', 'test');
+define('PASS', '12345');
 define('DB', 'moveit');
 
 include "../engine/functions.php";
